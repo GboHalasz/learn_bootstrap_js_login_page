e31de15c847cc33bbd8e03f7aba22ef74947590b
Gabor Halasz <hal2297@gmail.com>
2023-02-17 20:39:57 +0100

    Create an object and add registration form properties

--- login.js
+++ login.js
@@ -1,1 +1,36 @@
-/* Login - Registration page JS */
+/* Login - Registration page validation JS */
+
+const myRegForm = {
+    inputFields: {
+        regUname: {
+            elem: document.getElementById("regUName"),
+            value: "",                          //assigned on focusout                       
+            condition: (this.value.length >= 3)  //trim value before checking
+        },
+        regEmail: {
+            elem: document.getElementById("regEmail"),
+            value: "",
+            condition: (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.value))
+        },
+        regPass1: {
+            elem: document.getElementById("regEmail"),
+            value: "",
+            condition: (this.value.length >= 6)
+        },
+        regPass2: {
+            elem: document.getElementById("regEmail"),
+            value: "",
+            condition: (this.value === regPass1.value)
+        }
+    },
+
+    regBtn: document.getElementById("regBtn"),
+
+    
+
+
+
+}
+
+
+
