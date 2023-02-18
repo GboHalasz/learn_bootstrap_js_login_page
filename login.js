86954807b15e24a829768257e73c134638220395
GboHalasz <hal2297dev@gmail.com>
2023-02-18 18:42:13 +0100

    delete elem properties from myRegForm, modify regPass1 isValid function to handle the situation if the password was modified after the confirmation, modify listeners attachments

--- login.js
+++ login.js
@@ -1,5 +1,7 @@
 /* Login - Registration page validation JS */
 
+//the bootstrap extracts the input element by id!!
+
 class MyFormError extends Error {
     constructor(...params) {
         super(...params);
@@ -10,28 +12,26 @@ class MyFormError extends Error {
 const myRegForm = {
     inpFields: {
         regUName: {  //this must be equal with the id of input element!!!
-            elem: document.getElementById("regUName"),
             value: "",                          //assigned on focusout                       
             isValid: function (val) {
                 return val && (val.length >= 3)  //trim value before checking
             }
         },
         regEmail: {
-            elem: document.getElementById("regEmail"),
             value: "",
             isValid: function (val) {
-               return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val))
+                return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val))
             }
         },
         regPass1: {
-            elem: document.getElementById("regPass1"),
             value: "",
-            isValid: function (val) {
-               return val && (val.length >= 6)
+            isValid: function (val) {                
+                regPass2.value = "";                     //these are for the case when  
+                myRegForm.inpFields.regPass2.value = ""  //confirm password was given first
+                return val && (val.length >= 6);
             }
         },
         regPass2: {
-            elem: document.getElementById("regPass2"),
             value: "",
             isValid: function (val) {
                 return (val === regPass1.value)
@@ -41,7 +41,7 @@ const myRegForm = {
 
     regBtn: document.getElementById("regBtn"),
 
-    setValue: function (inp) {
+    setValueFromInp: function (inp) {
         this.inpFields[inp.id].value = this.checkField(inp);
         console.log(this.inpFields[inp.id].value)
     },
@@ -69,20 +69,20 @@ const myRegForm = {
     }
 }
 
-myRegForm.inpFields.regUName.elem.addEventListener("focusout", function () {
-    myRegForm.setValue(this);
+regUName.addEventListener("focusout", function () { //the bootstrap extracts the input element by Id
+    myRegForm.setValueFromInp(this);
 })
 
-myRegForm.inpFields.regEmail.elem.addEventListener("focusout", function () {
-    myRegForm.setValue(this);
+regEmail.addEventListener("focusout", function () {
+    myRegForm.setValueFromInp(this);
 })
 
-myRegForm.inpFields.regPass1.elem.addEventListener("focusout", function () {
-    myRegForm.setValue(this);
+regPass1.addEventListener("focusout", function () {
+    myRegForm.setValueFromInp(this);
 })
 
-myRegForm.inpFields.regPass2.elem.addEventListener("focusout", function () {
-    myRegForm.setValue(this);
+regPass2.addEventListener("focusout", function () {
+    myRegForm.setValueFromInp(this);
 })
 
 
