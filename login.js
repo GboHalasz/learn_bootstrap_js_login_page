10a4ddbd3956d02a9ef7b4cb693408c0b3b1e785
Gabor Halasz <hal2297@gmail.com>
2023-02-19 11:52:10 +0100

    replace the bootstrap word to browser, replace the email regex with RFC2822 compilant

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 /* Login - Registration page validation JS */
 
-//the bootstrap extracts the input element by id!!
+//the browser extracts the input element by id!!
 
 class MyFormError extends Error {
     constructor(...params) {
@@ -20,7 +20,9 @@ const myRegForm = {
         regEmail: {
             value: "",
             isValid: function (val) {
-                return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val))
+                // return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(val)) //from w3resource
+                //regex from masteringjs.io
+                return (/(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i.test(val))
             }
         },
         regPass1: {
@@ -88,14 +90,14 @@ const myRegForm = {
 
     attachListener: function (ev) {
         try {
-            if (this.inpFields.length != 0) { 
-            for (const field in this.inpFields) {                
-                window[field].addEventListener(ev, function () {
-                    myRegForm.setValueFromInp(this);
-                    myRegForm.valuesAreReady() ? myRegForm.enableRegBtn() : myRegForm.disableRegBtn();
-                })
+            if (this.inpFields.length != 0) {
+                for (const field in this.inpFields) {
+                    window[field].addEventListener(ev, function () {
+                        myRegForm.setValueFromInp(this);
+                        myRegForm.valuesAreReady() ? myRegForm.enableRegBtn() : myRegForm.disableRegBtn();
+                    })
+                }
             }
-        }
 
         } catch (err) {
             console.error(`${err.name}: ${err.message}`);
