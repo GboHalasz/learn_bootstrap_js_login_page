a39c09f1537c4d58a4600ec1b5db4a52727be269
GboHalasz <hal2297dev@gmail.com>
2025-05-20 22:31:37 +0200

    Set up Jest for unit testing and add initial tests for user.js
    - Installed Jest as a dev dependency
    - Configured basic test environment
    - Added first unit tests for user.js to verify user name handling and storeData event updates

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
     "presets": [
         ["@babel/preset-env", {
             "targets": "> 0.25%, not dead",
-            "modules": false,
             "useBuiltIns": "usage",
             "corejs": 3
         }
